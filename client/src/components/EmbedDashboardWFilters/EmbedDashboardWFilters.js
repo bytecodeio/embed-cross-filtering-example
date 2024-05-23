1556b86bad371db46ae93dfc62700d72e3383352
Elizabeth <elizabeth@Elizabeths-iMac.local>
2024-05-23 13:29:16 -0400

    updates

--- client/src/components/EmbedDashboardWFilters/EmbedDashboardWFilters.js
+++ client/src/components/EmbedDashboardWFilters/EmbedDashboardWFilters.js
@@ -30,6 +30,7 @@ const EmbedDashboardWFilters = () => {
   // State for the filter values, selected by the filter components located outside the embedded dashboard
   const [filterValues, setFilterValues] = React.useState({});
 
+
   // Looker API call using the API SDK to get all the available filters for the embedded dashboard
   useEffect(() => {
     const initialize = async () => {
@@ -110,7 +111,7 @@ const EmbedDashboardWFilters = () => {
           });
         })
 
-        .withParams({ _theme: '{"show_filters_bar":false}' })
+        // .withParams({ _theme: '{"show_filters_bar":false}' })
         .build()
         .connect()
         .then((response) => {
@@ -241,6 +242,12 @@ export const DashFilters2 = ({
     filter,
     sdk,
   });
+
+
+
+    // const filterType = ["day_range_picker"]
+    //
+    // filterType.includes(filter.ui_config.type)
 
   const FilterLabel = styled.span`
     font-family: inherit;
